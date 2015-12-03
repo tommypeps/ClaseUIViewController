f9985c525c4b21104084b856e670e8c52b9df880
José Roldán Jiménez <nosoycomplicao@gmail.com>
2015-12-04 00:39:50 +0100

    navigation view Push and pop

--- ClaseUIViewController/ClaseUIViewController/testViewController.m
+++ ClaseUIViewController/ClaseUIViewController/testViewController.m
@@ -24,6 +24,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)touch:(id)sender {
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
+
 /*
 #pragma mark - Navigation
 
